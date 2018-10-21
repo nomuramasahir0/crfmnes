8b359d71d6ec599725addfc9651ca0aaabf3d1ad
nmasahiro <masahironomura5325@gmail.com>
2018-10-22 04:31:01 +0900

    remove redundant kwargs

--- tests/test_rastrigin.py
+++ tests/test_rastrigin.py
@@ -21,7 +21,7 @@ def test_run_d40_rastrigin():
     allowable_evals = (148 + 8.8*3) * 1e3 # 2 sigma
     iteration_number = int(allowable_evals / lamb) + 1
 
-    cr = CRFMNES(dim, rastrigin, mean, sigma, lamb, use_constraint_violation=False)
+    cr = CRFMNES(dim, rastrigin, mean, sigma, lamb)
     x_best, f_best = cr.optimize(iteration_number)
     print("f_best:{}".format(f_best))
     assert f_best < 1e-12
@@ -36,7 +36,7 @@ def test_run_d80_rastrigin():
     allowable_evals = (296 + 8.0*3) * 1e3 # 2 sigma
     iteration_number = int(allowable_evals / lamb) + 1
 
-    cr = CRFMNES(dim, rastrigin, mean, sigma, lamb, use_constraint_violation=False)
+    cr = CRFMNES(dim, rastrigin, mean, sigma, lamb)
     x_best, f_best = cr.optimize(iteration_number)
     print("f_best:{}".format(f_best))
     assert f_best < 1e-12
